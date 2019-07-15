e68b69504a86845c3cabce1977cce366a1b011e2
Nguyen Quoc Viet <nguyenqviet98@gmail.com>
2019-07-15 21:13:15 +0630

    some fixes

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -11,9 +11,13 @@ public class CharacterMovement : MonoBehaviour
     public float maxVelocityY = 5F;
     public float minVelocityXAsRun = 0.5F;
     public float minVelocityYAsAir = 2F;
+
+    private int hMovement;
     private Animator animator;
     private Rigidbody2D rb;
     private bool disableMovement;
+    private bool jumpUp;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,46 +35,91 @@ public class CharacterMovement : MonoBehaviour
         }
         else
         {
+            jumpUp = false;
+            hMovement = 0;
             if (Input.GetKey(KeyCode.LeftArrow))
             {
-                rb.AddForce(new Vector2(-speed, 0));
-                GetComponent<SpriteRenderer>().flipX = true;
+                hMovement = -1;
             }
-            else if (Input.GetKey(KeyCode.RightArrow))
+
+            if (Input.GetKey(KeyCode.RightArrow))
             {
-                rb.AddForce(new Vector2(+speed, 0));
-                GetComponent<SpriteRenderer>().flipX = false;
-            }
+                hMovement = 1;
+            } 
 
             // Space pressed and not moving up or down
             if (Input.GetKeyDown(KeyCode.Space) && Math.Abs(rb.velocity.y) == 0)
             {
-                rb.AddForce(new Vector2(0, jumpSpeed));
+                jumpUp = true;
             }
 
-            SetCharState();
-            LimitCharVelocity();
+            
         }
 
 
+    }
+
+    private void FixedUpdate()
+    {
+        switch (hMovement)
+        {
+            case 1: MoveRight(); break;
+            case -1: MoveLeft(); break;
+            default: break;
+        }
+
+        if (jumpUp)
+        {
+            JumpUp();
+        }
+
+        LimitCharVelocity();
+        SetCharState();
+    }
+    void MoveLeft()
+    {
+        //rb.AddForce(Vector2.left * speed, ForceMode2D.Force);
+        rb.velocity = new Vector2(-speed, rb.velocity.y);
+        GetComponent<SpriteRenderer>().flipX = true;
+        Debug.Log("Running Left at " + rb.velocity);
+        //rb.MovePosition(rb.position + (Vector2.left*speed) * Time.deltaTime);
+    }
+
+    void MoveRight()
+    {
+        //rb.AddForce(Vector2.right * speed, ForceMode2D.Force);
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+        GetComponent<SpriteRenderer>().flipX = false;
+        Debug.Log("Running Right at " + rb.velocity);
+        //rb.MovePosition(rb.position + (Vector2.right * speed) * Time.deltaTime);
+    }
+
+    void JumpUp()
+    {
+        //rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
     }
 
     void LimitCharVelocity()
     {
-        if (Math.Abs(rb.velocity.x) > maxVelocityX)
-        {
-            rb.velocity = new Vector2(maxVelocityX * Math.Sign(rb.velocity.x), rb.velocity.y);
-        }
+        //if (Math.Abs(rb.velocity.x) > maxVelocityX)
+        //{
+        //    Debug.Log("Before x limmiter: " + rb.velocity);
+        //    rb.velocity = new Vector2(maxVelocityX * Math.Sign(rb.velocity.x), rb.velocity.y);
+        //    Debug.Log("After x limmiter: " + rb.velocity);
+        //}
 
-        if (Math.Abs(rb.velocity.y) > maxVelocityY)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, maxVelocityY * Math.Sign(rb.velocity.y));
-        }
+        //if (Math.Abs(rb.velocity.y) > maxVelocityY)
+        //{
+        //    Debug.Log("Before y limmiter: " + rb.velocity);
+        //    rb.velocity = new Vector2(rb.velocity.x, maxVelocityY * Math.Sign(rb.velocity.y));
+        //    Debug.Log("After y limmiter: " + rb.velocity);
+        //}
     }
     void SetCharState()
     {
-        Debug.Log(rb.velocity.y);
-        if (Math.Abs(rb.velocity.x) >= minVelocityXAsRun)
+        //Debug.Log(rb.velocity.y);
+        if (Math.Abs(rb.velocity.x) >= minVelocityXAsRun && !animator.GetBool("isAir"))
         {
             animator.SetBool("isRunning", true);
         }
@@ -79,7 +128,7 @@ public class CharacterMovement : MonoBehaviour
             animator.SetBool("isRunning", false);
         }
 
-        if (rb.velocity.y >= minVelocityYAsAir)
+        if (rb.velocity.y > minVelocityYAsAir)
         {
             animator.SetBool("isJumping", true);
             animator.SetBool("isAir", true);
